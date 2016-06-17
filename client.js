b41f9973bc5a40fe19cd57747dfa9530bb867afb
Marta Szczepaniak <mart.szcz@gmail.com>
2016-06-17 19:28:11 +0200

    add checking if string can be parsed to json. sending answer to server and checking it

--- client.js
+++ client.js
@@ -8,28 +8,53 @@ const rl = readline.createInterface({
 
 let client = new net.Socket();
 
+function isJson(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
 function onConnectionSuccess() {
 	console.log('Successfully connected to server!');
 	setTimeout(() => sendMessage('question'), 500);
+	//setTimeout(() => sendMessage('elo'), 3000);
 }
 
 function onReceiveData(data) {
-	let  message = JSON.parse(data);
-	let questionContent = message.question1.question;
-	let questionNumber = message.question1.number;
-	switch(questionNumber) {
-	    case '1':
-	    	console.log(`Received message ${data.toString()}\n`);
-	    	console.log(questionContent);
-	        rl.question( questionContent, (answer) => {
-	        	console.log(answer);
-	        	//client.write('aB');
-  				rl.close();
-			});
-	        break;
-	    default:
-	        console.log(`Received message ${data.toString()}\n`);
-	    }
+	if (isJson(data)) {
+		let json = JSON.parse(data);
+		let JSONdata;
+		for (var property in json) {
+    		if (json.hasOwnProperty(property)) {
+        		JSONdata = property;
+        		break;
+    		}
+		}
+		console.log(JSONdata);
+		switch(JSONdata) {
+			case 'question1':
+				let  message = JSON.parse(data);
+				let questionContent = message.question1.question;
+				let questionPossibleAnswers = message.question1.possible_answers;
+				let questionNumber = message.question1.number;
+				if(questionNumber == 1) {
+			    	console.log(`Received message ${data.toString()}\n`);
+			    	console.log(questionContent);
+			        rl.question( `${questionContent} ${questionPossibleAnswers}\n` , (answer) => {
+			        	client.write(`a${answer}`);
+		  				rl.close();
+					});
+				}
+			break;
+			case 'user':
+			break;
+		}
+	} else {
+	    console.log(`Received message ${data.toString()}\n`);
+	}
 }
 
 function sendMessage(message) {
--- server.c
+++ server.c
@@ -14,119 +14,119 @@
 char buffer[BUFFER_SIZE];
 
 struct Exam_question {
-  char number[2];
-  char question[100];
-  char possible_answers[100];
-  char correct_answer;
+	char number[2];
+	char question[100];
+	char possible_answers[100];
+	char correct_answer;
 };
 
 struct Exam {
-  struct Exam_question Q1;
-  struct Exam_question Q2;
-  struct Exam_question Q3;
-  char exam_json[500];
+	struct Exam_question Q1;
+	struct Exam_question Q2;
+	struct Exam_question Q3;
+	char exam_json[500];
 };
 
 
 void start_server(int sock) {
-  int one = 1;
-  struct sockaddr_in svr_addr;
-  if (sock < 0)
-    err(1, "can't open socket");
- 
-  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
- 
-  int port = 6666;
-  svr_addr.sin_family = AF_INET;
-  svr_addr.sin_addr.s_addr = INADDR_ANY;
-  svr_addr.sin_port = htons(port);
- 
-  if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
-    close(sock);
-    err(1, "Can't bind");
-  }
- 
-  listen(sock, 5);
+	int one = 1;
+	struct sockaddr_in svr_addr;
+	if (sock < 0)
+	err(1, "can't open socket");
+
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
+
+	int port = 6666;
+	svr_addr.sin_family = AF_INET;
+	svr_addr.sin_addr.s_addr = INADDR_ANY;
+	svr_addr.sin_port = htons(port);
+
+	if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
+	close(sock);
+	err(1, "Can't bind");
+	}
+
+	listen(sock, 5);
 }
 
 char* create_json_string(struct Exam Ex) {
-  char* json = malloc(500* sizeof(char));
-  sprintf(json, "{ \"question%s\": { \"number\": \"%s\", \"question\": \"%s\", \"possible_answers\": \"%s\", \"correct_answer\": \"%c\" } }", 
-    Ex.Q1.number, Ex.Q1.number, Ex.Q1.question, Ex.Q1.possible_answers, Ex.Q1.correct_answer);
-  return json;
+  	char* json = malloc(500* sizeof(char));
+  	sprintf(json, "{ \"question%s\": { \"number\": \"%s\", \"question\": \"%s\", \"possible_answers\": \"%s\", \"correct_answer\": \"%c\" } }", 
+	Ex.Q1.number, Ex.Q1.number, Ex.Q1.question, Ex.Q1.possible_answers, Ex.Q1.correct_answer);
+  	return json;
 }
 
 void mainHandler(connection) {
 
-  struct Exam_question Question1;
+  	struct Exam_question Question1;
 
-  strcpy( Question1.number, "1");
-  strcpy( Question1.question, "What color is a chalkboard?");
-  strcpy( Question1.possible_answers, "A - pink, B - green, C - yellow.");
-  Question1.correct_answer = 'B';
+ 	strcpy( Question1.number, "1");
+  	strcpy( Question1.question, "What color is a chalkboard?");
+  	strcpy( Question1.possible_answers, "A - pink, B - green, C - yellow.");
+  	Question1.correct_answer = 'B';
 
-  struct Exam Exam1;
-  Exam1.Q1 = Question1;
-  strcpy( Exam1.exam_json, create_json_string(Exam1));
-  printf("%s\n", Exam1.exam_json );
+  	struct Exam Exam1;
+  	Exam1.Q1 = Question1;
+  	strcpy( Exam1.exam_json, create_json_string(Exam1));
+  	printf("%s\n", Exam1.exam_json );
 	char response[BUFFER_SIZE];
 	char message[20];
 	char answer[5];
 	char result[BUFFER_SIZE];
   
-  // READ MESSAGE
-  int received_msg_length = read(connection, buffer, BUFFER_SIZE);
+  	// READ MESSAGE
+  	int received_msg_length = read(connection, buffer, BUFFER_SIZE);
 
 	while(received_msg_length > 0) {
-  	printf("buffer %s\n", buffer);
-  	printf("buffer length %d\n", received_msg_length);
+	printf("buffer %s\n", buffer);
+	printf("buffer length %d\n", received_msg_length);
 
-    
-    sprintf(message, "%s", buffer);
-    
-    // ASSIGN HANDLER
-    if(strcmp(message, "question") == 0) {
-      	strcpy(response, Exam1.exam_json);
-      	printf("response1:%s\n", response);
-    } else if(message[0] == 'a') {
-    	strcpy(answer, message);
-    	memmove (answer, answer + 1, strlen(answer));
-    	printf("msg:%s\n", answer);
-    	strcpy(response, answer);
-      strcpy(result, answer);
-      printf("RESULT:%s\n", result);
-      if(result[0] == 'B') {
-        printf("correct!");
-      }
-      printf("response3:%s\n", answer);
-    } else {
-    	strcpy(response, message);
-    	printf("response4:%s\n", message);
-    }
-    // RESPOND
-    memset(buffer, 0, strlen(buffer));
-    memset(message, 0, strlen(message));
-    write(connection, response, strlen(response));
-    // CHECK FOR NEW MESSAGES
-    received_msg_length = read(connection, buffer, BUFFER_SIZE);
+	
+	sprintf(message, "%s", buffer);
+	
+	// ASSIGN HANDLER
+	if(strcmp(message, "question") == 0) {
+		strcpy(response, Exam1.exam_json);
+		printf("response1:%s\n", response);
+	} else if(message[0] == 'a') {
+		strcpy(answer, message);
+		memmove (answer, answer + 1, strlen(answer)); //assign to answer message without first character
+		strcpy(response, answer);
+	  	strcpy(result, answer);
+	  	printf("RESULT:%s\n", result);
+	  	if(result[0] == 'B') {
+			strcpy(response, "correct!");
+	 	}
+	  	printf("response3:%s\n", answer);
+	} else {
+		strcpy(response, message);
+		printf("response4:%s\n", message);
+	}
+	//RESET buffers
+	memset(buffer, 0, strlen(buffer));
+	memset(message, 0, strlen(message));
+	// RESPOND
+	write(connection, response, strlen(response));
+	// CHECK FOR NEW MESSAGES
+	received_msg_length = read(connection, buffer, BUFFER_SIZE);
   }
 }
 
 int main() {
 
-  int client_socket_connection;
-  struct sockaddr_in cli_addr;
-  socklen_t sin_len = sizeof(cli_addr);
-  int sock = socket(AF_INET, SOCK_STREAM, 0);
-  
-  start_server(sock);
+	int client_socket_connection;
+	struct sockaddr_in cli_addr;
+	socklen_t sin_len = sizeof(cli_addr);
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
 
-  while (1) {
-    client_socket_connection = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
-    printf("got connection\n");
-    
-    mainHandler(client_socket_connection);
+	start_server(sock);
 
-    close(client_socket_connection);
-  }
+	while (1) {
+		client_socket_connection = accept(sock, (struct sockaddr *) &cli_addr, &sin_len);
+		printf("got connection\n");
+
+		mainHandler(client_socket_connection);
+
+		close(client_socket_connection);
+  	}
 }
