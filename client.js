cb43ac0963f200a9c6ccb00c25500d05981ab7f1
Marta Szczepaniak <mart.szcz@gmail.com>
2016-06-19 00:15:41 +0200

    new implementation

--- client.js
+++ client.js
@@ -1,65 +1,162 @@
 const net = require('net');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const inquirer = require('inquirer');
+const randomString = require('randomstring');
+const _ = require('lodash');
 
 let client = new net.Socket();
 
-function isJson(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
+let messages = {};
+
+function promptLogIn() {
+	const questionList = [
+		{type: 'input', name: 'login', message: 'Your login:'},
+		{type: 'password', name: 'password', message: 'Your password:'}
+	]
+
+	inquirer
+		.prompt(questionList)
+		.then(credentials => logIn(credentials));
+}
+
+function promptStudentsChoice() {
+	const choiceList = [
+		{
+			type: 'list', 
+			name: 'choice' , 
+			message: 'Pick action:', 
+			choices: ['Start exam.', 'See exam results.']
+		}
+	]
+
+	inquirer
+		.prompt(choiceList)
+		.then((answer) => (getExam()));
+}
+
+function promptTeachersChoice() {
+	const choiceList = [
+		{
+			type: 'list', 
+			name: 'choice' , 
+			message: 'Pick action:', 
+			choices: ['Add exam.', 'See students results.']
+		}
+	]
+
+	inquirer
+		.prompt(choiceList)
+		.then((answer) => (promptAddQuestion()));
+}
+
+function promptAddQuestion() {
+	const questionList = [
+		{type: 'input', name: 'question', message: 'Input content of question and possible answers:'},
+		{type: 'input', name: 'correctAnswer', message: 'Input correct answer(e.g. A):'}
+	]
+
+	inquirer
+		.prompt(questionList)
+		.then(answer => {
+			console.log(answer);
+			addExam(answer);
+		});
+}
+
+function promptQuestion(question) {
+	const choiceList = [
+		{
+			type: 'list', 
+			name: 'choice' , 
+			message: 'Your answer is:', 
+			choices: ['A', 'B', 'C']
+		}
+	]
+	console.log(question);
+	inquirer.prompt(choiceList).then(
+		({choice}) => sendMessage('answer', choice))
 }
 
 function onConnectionSuccess() {
-	console.log('Successfully connected to server!');
-	setTimeout(() => sendMessage('question'), 500);
-	//setTimeout(() => sendMessage('elo'), 3000);
+	// console.log('Successfully connected to server!\n');
+	promptLogIn();
 }
 
-function onReceiveData(data) {
-	if (isJson(data)) {
-		let json = JSON.parse(data);
-		let JSONdata;
-		for (var property in json) {
-    		if (json.hasOwnProperty(property)) {
-        		JSONdata = property;
-        		break;
-    		}
-		}
-		console.log(JSONdata);
-		switch(JSONdata) {
-			case 'question1':
-				let  message = JSON.parse(data);
-				let questionContent = message.question1.question;
-				let questionPossibleAnswers = message.question1.possible_answers;
-				let questionNumber = message.question1.number;
-				if(questionNumber == 1) {
-			    	console.log(`Received message ${data.toString()}\n`);
-			    	console.log(questionContent);
-			        rl.question( `${questionContent} ${questionPossibleAnswers}\n` , (answer) => {
-			        	client.write(`a${answer}`);
-		  				rl.close();
-					});
+function onReceiveData(jsonData) {
+	console.log(jsonData.toString());
+
+	const data = JSON.parse(jsonData);
+	const {uuid, status, user_type} = data;
+	const {action} = messages[uuid];
+
+	if(status !== 'ok') console.log(`\n ${status} \n`);
+
+	switch(action) {
+		case 'logIn': {
+			if (status === 'ok') {
+				switch(user_type) {
+					case 'student':
+						promptStudentsChoice();
+					break;
+					case 'teacher':
+						promptTeachersChoice();
+					break;
 				}
-			break;
-			case 'user':
+			} else {
+				promptLogIn();
+			}
 			break;
 		}
-	} else {
-	    console.log(`Received message ${data.toString()}\n`);
+		case 'getExam': {
+			if(status === 'ok') {
+				const {question} = data;
+				promptQuestion(question);
+			}
+			break;
+		}
+		case 'answer': {
+			if(status === 'ok') {
+				const {question, score} = data;
+				if(score) {
+					console.log(`\nYour score is: ${score}\n`); 
+					promptStudentsChoice();
+				} else { 
+					promptQuestion(question); 
+				}
+			}
+			break;		
+		}
 	}
+	delete(messages[uuid]);
 }
 
-function sendMessage(message) {
-	console.log(`Sent message ${message}`);
+const actions = {
+	logIn: 0,
+	getExam: 1,
+	answer: 2,
+	addExam: 3,
+}
+
+function sendMessage(action, payload = '') {	
+	const UUID = randomString.generate(8);
+	const message = `${actions[action]}${UUID}${payload}`;
+
 	client.write(message);
+	messages[UUID] = {action, message};
+	console.log(`Sent message ${message}`);
+}
+
+function logIn({login, password}) {
+	const payload = `${_.padEnd(login, 8)}${_.padEnd(password, 8)}`;
+	sendMessage('logIn', payload);
+}
+
+function getExam() {
+	sendMessage('getExam');
+}
+
+function addExam({question, correctAnswer}) {
+	const payload = `${_.padEnd(question, 100)}${correctAnswer}`;
+	sendMessage('addExam', payload);
 }
 
 client.on('data', onReceiveData);
--- server.c
+++ server.c
@@ -9,30 +9,40 @@
 #include <arpa/inet.h>
 #include <err.h>
 
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 1000
 
-char buffer[BUFFER_SIZE];
-
-struct Exam_question {
-	char number[2];
-	char question[100];
-	char possible_answers[100];
+struct Question {
+	char content[200];
 	char correct_answer;
 };
 
 struct Exam {
-	struct Exam_question Q1;
-	struct Exam_question Q2;
-	struct Exam_question Q3;
-	char exam_json[500];
+	int current_question;
+	struct Question questions[5];
+	char answers[5];
 };
 
+struct User {
+	char type[10];
+	char login[9];
+	char password[9];
+	struct Exam exam;
+};
+
+struct User_exams {
+	struct Exam exam;
+	struct User user;
+};
+
+struct User users[10];
+struct User_exams user_exams;
+
+unsigned char buffer[BUFFER_SIZE];
 
 void start_server(int sock) {
 	int one = 1;
 	struct sockaddr_in svr_addr;
-	if (sock < 0)
-	err(1, "can't open socket");
+	if (sock < 0) err(1, "can't open socket");
 
 	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int));
 
@@ -42,77 +52,157 @@ void start_server(int sock) {
 	svr_addr.sin_port = htons(port);
 
 	if (bind(sock, (struct sockaddr *) &svr_addr, sizeof(svr_addr)) == -1) {
-	close(sock);
-	err(1, "Can't bind");
+		close(sock);
+		err(1, "Can't bind");
 	}
 
 	listen(sock, 5);
 }
 
-char* create_json_string(struct Exam Ex) {
-  	char* json = malloc(500* sizeof(char));
-  	sprintf(json, "{ \"question%s\": { \"number\": \"%s\", \"question\": \"%s\", \"possible_answers\": \"%s\", \"correct_answer\": \"%c\" } }", 
-	Ex.Q1.number, Ex.Q1.number, Ex.Q1.question, Ex.Q1.possible_answers, Ex.Q1.correct_answer);
-  	return json;
+char* create_response_with_question(char* response, char* uuid, char* question) {
+	sprintf(response, 
+		"{\"uuid\": \"%s\", \"status\": \"ok\", \"question\": \"%s\"}",
+		uuid, question
+	);
+	return response;
+}
+
+struct Exam create_exam() {
+	struct Question question1 = {"What color is a chalkboard? A - pink, B - green, C - yellow.", 'B'};
+	struct Question question2 = {"What color is a dog? A - pink, B - green, C - yellow.", 'A'};
+	struct Exam exam = {0};
+	exam.questions[0] = question1;
+	exam.questions[1] = question2;
+	return exam;
 }
 
 void mainHandler(connection) {
+	//INITIALIZING BUFFER
+	unsigned char response[BUFFER_SIZE];
 
-  	struct Exam_question Question1;
+	int current_user = 0;
+	struct Exam current_exam;
 
- 	strcpy( Question1.number, "1");
-  	strcpy( Question1.question, "What color is a chalkboard?");
-  	strcpy( Question1.possible_answers, "A - pink, B - green, C - yellow.");
-  	Question1.correct_answer = 'B';
-
-  	struct Exam Exam1;
-  	Exam1.Q1 = Question1;
-  	strcpy( Exam1.exam_json, create_json_string(Exam1));
-  	printf("%s\n", Exam1.exam_json );
-	char response[BUFFER_SIZE];
-	char message[20];
-	char answer[5];
-	char result[BUFFER_SIZE];
-  
-  	// READ MESSAGE
-  	int received_msg_length = read(connection, buffer, BUFFER_SIZE);
+	// READ MESSAGE
+	int received_msg_length = read(connection, buffer, BUFFER_SIZE);
 
 	while(received_msg_length > 0) {
-	printf("buffer %s\n", buffer);
-	printf("buffer length %d\n", received_msg_length);
+		char uuid[9];
+		memcpy(uuid, &buffer[1], 8);
+		uuid[8] = '\0';
 
-	
-	sprintf(message, "%s", buffer);
-	
-	// ASSIGN HANDLER
-	if(strcmp(message, "question") == 0) {
-		strcpy(response, Exam1.exam_json);
-		printf("response1:%s\n", response);
-	} else if(message[0] == 'a') {
-		strcpy(answer, message);
-		memmove (answer, answer + 1, strlen(answer)); //assign to answer message without first character
-		strcpy(response, answer);
-	  	strcpy(result, answer);
-	  	printf("RESULT:%s\n", result);
-	  	if(result[0] == 'B') {
-			strcpy(response, "correct!");
-	 	}
-	  	printf("response3:%s\n", answer);
-	} else {
-		strcpy(response, message);
-		printf("response4:%s\n", message);
+		// ASSIGN HANDLER
+		switch(buffer[0]) {
+			case '0': {
+				char login[9];
+				char password[9];
+
+				memcpy(login, &buffer[9], 8);
+				login[8] = '\0';
+
+				memcpy(password, &buffer[17], 8);
+				password[8] = '\0';
+				int user_index;
+				for(user_index = 0; user_index < 10; user_index++) {
+					if(strcmp(login, users[user_index].login) == 0 && 
+						strcmp(password, users[user_index].password) == 0) {
+						current_user = user_index;
+						sprintf(response, "{\"uuid\": \"%s\", \"status\": \"ok\", \"user_type\": \"%s\"}", 
+							uuid, users[user_index].type);
+						break;
+					} else {
+						//printf("\n\n%s\n", users[1].login);
+						sprintf(response, "{\"uuid\": \"%s\", \"status\": \"Incorrect credentials.\"}", uuid);
+					}
+				}
+				break; 
+			}
+			case '1': {
+				if(current_user != 0) {
+					users[current_user].exam = user_exams.exam;
+					current_exam = users[current_user].exam;
+
+					create_response_with_question(response, uuid, 
+						current_exam.questions[current_exam.current_question].content);
+				} else {
+					sprintf(response, "{\"uuid\": \"%s\", \"status\": \"Unauthorized access.\"}", uuid);
+				}
+				break;
+			}
+			case '2': {
+				if(current_user != 0) {
+					char answer = buffer[9];
+					current_exam.answers[current_exam.current_question] = answer;
+					printf("%d\n",  current_exam.current_question);
+					current_exam.current_question++;
+
+					if (current_exam.current_question < (sizeof(current_exam.questions)/sizeof(current_exam.questions[0]))) {
+						create_response_with_question(response, uuid, 
+						current_exam.questions[current_exam.current_question].content);
+					} else {
+						int score = 0;
+						int question_index;
+						for(question_index = 0; question_index < 5; question_index++) {
+							if(current_exam.questions[question_index].correct_answer == current_exam.answers[question_index])
+								score++;
+						}
+						sprintf(response, "{\"uuid\": \"%s\", \"status\": \"ok\", \"score\": \"%d\"}", uuid, score);
+					}
+				}
+				break;
+			}
+			case '3': {
+				struct Question question;
+				//char question_content[33];
+				//char correct_answer;
+
+				memcpy(question.content, &buffer[9], 100);
+				question.content[100] = '\0';
+
+				question.correct_answer = buffer[109];
+				int question_index;
+				for(question_index = 0; question_index < 5; question_index++) {
+					if(user_exams.exam.questions[question_index] == NULL) {
+						strcpy(user_exams.exam.questions[question_index].content, question.content);
+						user_exams.exam.questions[question_index].correct_answer = question.correct_answer;
+						break;
+					}
+				}
+				printf("Cos tam\n%s  %c\n", question.content, question.correct_answer);
+				strcpy(response, "received addExam");
+				break;
+			}
+			default:
+				strcpy(response, "Incorrect action number");
+		}
+
+		// RESPOND
+		write(connection, response, strlen(response));
+		
+		//RESET buffer
+		memset(buffer, 0, strlen(buffer));
+		memset(response, 0, strlen(response));
+		
+		// CHECK FOR NEW MESSAGES
+		received_msg_length = read(connection, buffer, BUFFER_SIZE);
 	}
-	//RESET buffers
-	memset(buffer, 0, strlen(buffer));
-	memset(message, 0, strlen(message));
-	// RESPOND
-	write(connection, response, strlen(response));
-	// CHECK FOR NEW MESSAGES
-	received_msg_length = read(connection, buffer, BUFFER_SIZE);
-  }
 }
 
 int main() {
+	struct User user0;
+
+	strcpy(user0.type, "student");
+	strcpy(user0.login, "login123");
+	strcpy(user0.password, "password");
+
+	struct User user1;
+
+	strcpy(user1.type, "teacher");
+	strcpy(user1.login, "teacher1");
+	strcpy(user1.password, "password");
+
+	users[1] = user0;
+	users[2] = user1;
 
 	int client_socket_connection;
 	struct sockaddr_in cli_addr;
