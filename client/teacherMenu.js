65d674e2dc536fc9b491c25d2d86207815c8ce59
Michał Warda <michal.warda@elpassion.pl>
2016-08-27 15:57:17 +0200

    Implement adding exam on frontend.

--- client/teacherMenu.js
+++ client/teacherMenu.js
@@ -14,9 +14,45 @@ const teacherMenuPrompt = () =>
     },
   ]);
 
-const addExamPrompt = () =>
-  inquirer.prompt([{ type: 'input', name: 'name', message: 'Exam name:' }]);
+const addQuestionPrompt = () =>
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'question',
+        message: 'Input content of question and possible answers:',
+      },
+      {
+        type: 'list',
+        name: 'correctAnwer',
+        message: 'Correct answer:',
+        choices: [
+          { name: 'A', value: 'A' },
+          { name: 'B', value: 'B' },
+          { name: 'C', value: 'C' },
+        ],
+      },
+    ]);
 
+const addExamPrompt = () =>
+  co(function* () {
+    const { examName } = yield inquirer.prompt([
+      { type: 'input', name: 'examName', message: 'Exam name:' },
+    ]);
+    let addNextQuestion = true;
+    const questions = [];
+
+    while (addNextQuestion) {
+      const question = yield addQuestionPrompt();
+      questions.push(question);
+      const { nextQuestion } = yield inquirer.prompt([
+        { type: 'confirm', name: 'nextQuestion', message: 'Add another question?' },
+      ]);
+      addNextQuestion = nextQuestion;
+    }
+
+    return { examName, questions };
+  });
 
 const teacherMenu = () =>
   co(function* () {
